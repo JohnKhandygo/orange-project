677f6814470f80793b9c798439cb708ea817309e
JohnKhandygo <johnkhandygo@gmail.com>
2015-11-22 21:43:19 +0300

    twitter users api implemented

---
+++ src/main/java/com/kspt/orange/frameworks/twitter/api/AuthenticatedUsersApi.java
@@ -0,0 +1,18 @@
+package com.kspt.orange.frameworks.twitter.api;
+
+import com.kspt.orange.frameworks.twitter.api.data.User;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("/account/verify_credentials.json")
+@Produces(MediaType.APPLICATION_JSON)
+public interface AuthenticatedUsersApi {
+
+  @GET
+  User authenticatedUser(
+      @QueryParam("include_entities") boolean includeEntities,
+      @QueryParam("skip_status") boolean skipStatus);
+}
